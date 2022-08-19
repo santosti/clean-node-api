6dc1df19298fd3c69f250d565e870ac193ed759c
santosti <sanntosti@gmail.com>
2022-08-18 23:04:45 -0300

    test: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,13 +1,32 @@
+import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamError } from '../errors/missing-param-error';
+import { EmailValidator } from '../protocols/email-validator';
 import { SignUpController } from './signup';
 
-const makeSut = (): SignUpController => {
-  return new SignUpController();
+interface SutTypes {
+  sut: SignUpController;
+  emailValidatorStub: EmailValidator;
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub();
+  const sut = new SignUpController(emailValidatorStub);
+
+  return {
+    sut,
+    emailValidatorStub,
+  };
 };
 
 describe('Signup Controller ', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: 'test@example.com',
@@ -16,14 +35,14 @@ describe('Signup Controller ', () => {
       },
     };
 
-    const httpResponde = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest);
 
-    expect(httpResponde.statusCode).toBe(400);
-    expect(httpResponde.body).toEqual(new MissingParamError('name'));
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
 
   test('should return 400 if no email is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'testname',
@@ -32,14 +51,14 @@ describe('Signup Controller ', () => {
       },
     };
 
-    const httpResponde = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest);
 
-    expect(httpResponde.statusCode).toBe(400);
-    expect(httpResponde.body).toEqual(new MissingParamError('email'));
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
 
   test('should return 400 if no password is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'testname',
@@ -48,14 +67,14 @@ describe('Signup Controller ', () => {
       },
     };
 
-    const httpResponde = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest);
 
-    expect(httpResponde.statusCode).toBe(400);
-    expect(httpResponde.body).toEqual(new MissingParamError('password'));
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('password'));
   });
 
   test('should return 400 if no passwordConfirmation is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'testname',
@@ -64,11 +83,31 @@ describe('Signup Controller ', () => {
       },
     };
 
-    const httpResponde = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest);
 
-    expect(httpResponde.statusCode).toBe(400);
-    expect(httpResponde.body).toEqual(
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
       new MissingParamError('passwordConfirmation')
     );
+  });
+
+  test('should return 400 if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut();
+
+    emailValidatorStub.isValid = jest.fn(() => false);
+
+    const httpRequest = {
+      body: {
+        name: 'testname',
+        email: 'invalid-email@example.com',
+        password: 'testpassword',
+        passwordConfirmation: 'testpassword',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
 });
