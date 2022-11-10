903ac191a16f6ee41d4f11364d167b249b6f2949
santosti <sanntosti@gmail.com>
2022-11-09 22:35:12 -0300

    feat: ensure SignUp route return an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import setUpMiddleware from './middlewares';
+import setUpRoutes from './routes';
 
 const app = express();
 
 setUpMiddleware(app);
+setUpRoutes(app);
 
 export default app;
