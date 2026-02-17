5673e0f1bc0aff7286ba45ce3fa71cca2b83f1ce
santosti <sanntosti@gmail.com>
2026-02-17 17:21:52 -0300

    feat: Ensure DbAuthentication calls hashCompare with a correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,3 +1,4 @@
+import { HashCompare } from '@/data/protocols/criptografhy/hash-compare';
 import { LoadAccountByEmailRepositoy } from '@/data/protocols/db/load-account-by-email-repository';
 import {
   Authentication,
@@ -6,11 +7,22 @@ import {
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepositoy;
-  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepositoy) {
+  private readonly hashCompare: HashCompare;
+
+  constructor(
+    loadAccountByEmailRepository: LoadAccountByEmailRepositoy,
+    hashCompare: HashCompare
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+    this.hashCompare = hashCompare;
   }
   async auth(authentication: AuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authentication.email);
+    const account = await this.loadAccountByEmailRepository.load(
+      authentication.email
+    );
+    if (account) {
+      await this.hashCompare.compare(authentication.password, account.password);
+    }
     return null;
   }
 }
